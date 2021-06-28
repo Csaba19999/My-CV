9300fe779bdfe8a50a71ed56bf1305a1b2430bb5
Csaba19999 <menesi777@gmail.com>
2021-06-28 22:45:33 +0200

    referencia card alap, hobbi-referencia linkelés

--- JS_tartalom.js
+++ JS_tartalom.js
@@ -1,18 +1,44 @@
 
 
-
 function DarkModeSwitch() {
-    var hatter = document.getElementById("myonoffswitch").checked;
+    var darkButton = document.getElementById("myonoffswitch");
 
-    if (hatter === true) {
-        hatter.classList.remove("lightM");
-        hatter.classList.add("darkM");
-        console.log("Sötét mód bekapcsolva.");
-    } else if (hatter === false) {
-        hatter.classList.remove("darkM");
-        hatter.classList.add("lightM");
-        console.log("Sötét mód kikapcsolva.");
-    } else {
-        console.log("Valami gond van a Dark móddal.");
+    if (darkButton.checked === true) {
+        var lightClass = document.getElementsByClassName("lightM");
+        for (var i = 0; i < lightClass.length; i + 2) {
+            lightClass[i].className = lightClass[i].className.replace("lightM", "darkM");
+            console.log("sötét mód be.");
+        }
+        addStyle();
+        function addStyle(styleString) {
+            const style = document.createElement('style');
+            style.textContent = styleString;
+            document.head.append(style);
+        }
+
+        addStyle(`
+        svg path {
+            stroke: white;
+        }
+        `);
+    }
+
+    if (darkButton.checked === false) {
+        var darkClass = document.getElementsByClassName("darkM");
+        for (var i = 0; i < darkClass.length; i + 2) {
+            darkClass[i].className = darkClass[i].className.replace("darkM", "lightM");
+            console.log("sötét mód ki.");
+        }
+        addStyle();
+        function addStyle(styleString) {
+            const style = document.createElement('style');
+            style.textContent = styleString;
+            document.head.append(style);
+        }
+        addStyle(`
+        svg path {
+            stroke: black;
+        }
+        `);
     }
 }
